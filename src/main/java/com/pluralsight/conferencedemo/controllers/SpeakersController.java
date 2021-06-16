87eeeaf1293501612e02227ee3bd572ff54ff449
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-06-16 17:49:25 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,39 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {return speakerRepository.getById(id);}
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) { return speakerRepository.saveAndFlush(speaker);}
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        //TODO: Add validation that all attributes are passed in
+        Speaker existingSpeaker = speakerRepository.getById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
